fad1ea518cf7f2e47706bc5667d049f87d6cb132
Sherif Kamal <s.kamal@aqwas.sa>
2021-02-05 06:32:01 +0200

    Initial Commit

---
+++ MTMsTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MTMsTask
+//
+//  Created by sherif kamal on 05/02/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
